4993db03dd2f454078ac30bdea543c0870791748
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2017-09-20 12:32:41 -0400

    changed indent to spaces

--- R/which.node.R
+++ R/which.node.R
@@ -14,24 +14,23 @@
 #' # find the root node:
 #' rnode <- which.node(tr, tr$tip.label)
 
-which.node <- function(phy, tips)
-{
-if (is.character(tips) == TRUE){
-	tips_n = vector("numeric",length(tips))
-	for (i in 1:length(tips))
-		tips_n[i] = which(phy$tip.label == tips[i])
-	rows = vector("numeric",length(tips_n))
-	for (i in 1:length(tips_n))
-		rows[i] = which(phy$edge[,2] == tips_n[i])
-	nodes = sort(phy$edge[rows[which.min(rows)]:rows[which.max(rows)],1])
-	return(nodes[1])
-} else if (is.numeric(tips) == TRUE){
-	rows = vector("numeric",length(tips))
-	for (i in 1:length(tips))
-		rows[i] = which(phy$edge[,2] == tips[i])
-	nodes = sort(phy$edge[rows[which.min(rows)]:rows[which.max(rows)],1])
-	return(nodes[1])
-} else if (is.null(tips) == TRUE){
-	stop('tips vector is empty')
-}
+which.node <- function(phy, tips){
+   if (is.character(tips) == TRUE){
+       tips_n = vector("numeric",length(tips))
+       for (i in 1:length(tips))
+           tips_n[i] = which(phy$tip.label == tips[i])
+       rows = vector("numeric",length(tips_n))
+       for (i in 1:length(tips_n))
+           rows[i] = which(phy$edge[,2] == tips_n[i])
+       nodes = sort(phy$edge[rows[which.min(rows)]:rows[which.max(rows)],1])
+       return(nodes[1])
+   } else if (is.numeric(tips) == TRUE){
+       rows = vector("numeric",length(tips))
+       for (i in 1:length(tips))
+           rows[i] = which(phy$edge[,2] == tips[i])
+       nodes = sort(phy$edge[rows[which.min(rows)]:rows[which.max(rows)],1])
+       return(nodes[1])
+   } else if (is.null(tips) == TRUE){
+       stop('tips vector is empty')
+   }
 }
