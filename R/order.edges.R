1daaa7bf4f97a432240d59de6f5fd925a6b2c176
Santiago Sanchez-Ramirez <santiago@Santiagos-Mac-mini.local>
2017-09-22 14:22:39 -0400

    added closing bracket to function: edited order.edges

--- R/order.edges.R
+++ R/order.edges.R
@@ -20,7 +20,7 @@
 
 order.edges <- function(phy){
     pp <- prop.part(phy)
-    edg <- sapply(pp, function(x,y=phy min(which.edge(y,x))-1 )
+    edg <- sapply(pp, function(x,y=phy) min(which.edge(y,x))-1 )
     edg <- edg[-1]
     return(edg)
 }
