a478d59152ef3f7d05a329fe9e4cd652e98d4843
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2018-03-02 17:07:27 -0500

    added roxygen headers

--- R/plot.phylo.HPD.R
+++ R/plot.phylo.HPD.R
@@ -1,5 +1,37 @@
-
-
+#' plot.phylo.HPD
+#'
+#' This function plots a \code{phylo} BEAST tree read by
+#' \code{read.beast.annot} with its 95% HPD node height
+#' intervals and adds a x-axis scale. 
+#'
+#' @param x annotated \code{phylo} object
+#' @param nodes a vector with node numbers to 
+#'              plot. Default is \code{NULL}
+#' @param bar.width fraction of 1 for bar thickness
+#' @param bar.col bar color
+#' @param border if bar borders should be drawn. See ?rect.
+#' @param ... further arguments passed by \code{phylo.plot}
+#' @return plot
+#' @seealso \code{\link{plot.phylo}} \code{\link{rect}}
+#' @export
+#' @examples
+#' file <- system.file("data/mcc.tre", package="rBt")
+#' tr <- read.beast.annot(file)
+#' # plot HPD heights on all nodes:
+#' plot.phylo.HPD(tr, cex=0.5, bar.width=0.4, bar.col="red", border=NA)
+#' # with library(scales) for transparency
+#' # plot.phylo.HPD(tr, cex=0.5, bar.width=0.4, bar.col=alpha("red",0.5), border=NA)
+#' # plot HPD heights on specific nodes:
+#' tips <- tr$tip.label
+#' tipsfac <- factor(sapply(tips, function(x) strsplit(x, "__")[[1]][1] ))
+#' names(tipsfac) <- NULL
+#' tipslist <- split(tips, tipsfac)
+#' allanc <- c(unlist(sapply(tipslist, function(x, y=tr) Ancestors(tr, which.node(y, x)) )), 
+#'             sapply(tipslist, function(x) which.node(tr, x)))
+#' allanc <- sort(allanc)
+#' allanc <- allanc[ !duplicated(allanc) ]
+#' names(allanc) <- NULL
+#' plot.phylo.HPD(tr, cex=0.5, bar.width=0.4, bar.col="red", border=NA, nodes=allanc)
 
 plot.phylo.HPD <- function(x, nodes=NULL, bar.width=0.3, bar.col=NA, border=NULL, ...){
 	plot(x, plot=F, ...)
